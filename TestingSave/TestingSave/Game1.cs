e3057f3001071e5693b6b0560b1c3917ba7fbc73
Stephen Fetinko <sfetinko@gmail.com>
2018-05-03 19:29:58 -0300

    Starting testing saving features

--- TestingSave/TestingSave/Game1.cs
+++ TestingSave/TestingSave/Game1.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using RogueSharp;
-using RogueSharp.MapCreation;
 
-namespace LearningMonoGame
+namespace TestingSave
 {
     /// <summary>
     /// This is the main type for your game.
@@ -14,19 +12,10 @@ namespace LearningMonoGame
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         
-        private Texture2D _wall;
-        private Texture2D _floor;
-
-        private IMap _map;
-
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-
-            IMapCreationStrategy<Map> mapCreationStrategy =
-                new RandomRoomsMapCreationStrategy<Map>(50, 30, 100, 7, 3);
-            _map = Map.Create(mapCreationStrategy);
         }
 
         /// <summary>
@@ -38,9 +27,7 @@ namespace LearningMonoGame
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            IMapCreationStrategy<Map> mapCreationStrategy =
-            new RandomRoomsMapCreationStrategy<Map>(50, 30, 100, 7, 3);
-            _map = Map.Create(mapCreationStrategy);
+
             base.Initialize();
         }
 
@@ -54,9 +41,7 @@ namespace LearningMonoGame
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            _wall = Content.Load<Texture2D>("Wall");
-            _floor = Content.Load<Texture2D>("Floor");
-        }   
+        }
 
         /// <summary>
         /// UnloadContent will be called once per game and is the place to unload
@@ -91,29 +76,6 @@ namespace LearningMonoGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-            spriteBatch.Begin();
-            spriteBatch.Draw(_wall, Vector2.Zero, Color.White);
-            spriteBatch.Draw(_floor, new Vector2(64, 64), Color.White);
-            spriteBatch.End();
-
-            spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend);
-
-            int sizeOfSprites = 64;
-            float scale = .25f;
-            foreach (Cell cell in _map.GetAllCells())
-            {
-                var position = new Vector2(cell.X * sizeOfSprites * scale, cell.Y * sizeOfSprites * scale);
-                if (cell.IsWalkable)
-                {
-                    spriteBatch.Draw(_floor, position, null, null, null, 0.0f, new Vector2(scale, scale), Color.White);
-                }
-                else
-                {
-                    spriteBatch.Draw(_wall, position, null, null, null, 0.0f, new Vector2(scale, scale), Color.White);
-                }
-            }
-
-            spriteBatch.End();
 
             base.Draw(gameTime);
         }
