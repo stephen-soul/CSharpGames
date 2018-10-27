1c398c377febe810c7a6e6f46e4997ccf903d0a5
Stephen <sfetinko@gmail.com>
2018-10-27 16:02:16 -0300

    Started console adventure

---
+++ xadventure/xadventure/Logic.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Handle The Logic For The Game.
+ * Stephen Fetinko
+ * 2018
+ */
+
+using System;
+
+namespace xadventure
+{
+    public class Logic
+    {
+		/// <summary>
+        /// Initialize the logic.
+        /// </summary>
+		public Logic()
+        {
+			
+        }
+        
+        /// <summary>
+        /// Starts a new game.
+        /// </summary>
+		public void StartNewGame()
+		{
+			
+		}
+
+        /// <summary>
+        /// Try to load the game.
+        /// </summary>
+		public void TryToLoadGame(string playerName)
+		{
+			
+		}
+    }
+}
---
+++ xadventure/xadventure/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Initialize the game.
+ * Stephen Fetinko
+ * 2018
+ */
+
+using System;
+
+namespace xadventure
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			Logic gameLogic = new Logic();
+			// If args exist, check for a save. Else, new game
+			if (args.Length > 0)
+			{
+				gameLogic.TryToLoadGame(args[0]);
+			}
+			else 
+			{
+				gameLogic.StartNewGame();
+			}
+        }
+    }
+}
